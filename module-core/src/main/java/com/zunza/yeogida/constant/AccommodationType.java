7a246f8094b0214d5966fc71ebf55e31123c4edb
zunza <fksfks015@naver.com>
2024-10-18 00:58:05 +0900

    feat: 숙소 타입을 위한 Enum 클래스 추가

---
+++ module-core/src/main/java/com/zunza/yeogida/constant/AccommodationType.java
@@ -0,0 +1,11 @@
+package com.zunza.yeogida.constant;
+
+public enum AccommodationType {
+	HOTEL("호텔"),
+	PENSION("펜션"),
+	GUESTHOUSE("게스트 하우스"),
+	MOTEL("모텔");
+
+	AccommodationType(String value) {
+	}
+}

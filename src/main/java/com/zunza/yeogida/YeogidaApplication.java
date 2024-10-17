6f40cec06bf10932d7122b3247119621a342df9a
zunza <fksfks015@naver.com>
2024-10-17 19:53:48 +0900

    CREATE NEW PROJECT

---
+++ src/main/java/com/zunza/yeogida/YeogidaApplication.java
@@ -0,0 +1,13 @@
+package com.zunza.yeogida;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YeogidaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YeogidaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zunza/yeogida/YeogidaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zunza.yeogida;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YeogidaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

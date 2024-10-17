89c9a795627fb228c0463ac4809247368c1232db
zunza <fksfks015@naver.com>
2024-10-18 00:33:54 +0900

    feat: Jpa 데이터 감사 설정 추가

---
+++ module-core/src/main/java/com/zunza/yeogida/config/DataConfig.java
@@ -0,0 +1,9 @@
+package com.zunza.yeogida.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class DataConfig {
+}

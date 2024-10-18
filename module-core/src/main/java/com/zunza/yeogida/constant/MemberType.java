ecf01d91c465acf0a2aa2835b6979ef5e9b25f00
zunza <fksfks015@naver.com>
2024-10-19 00:50:12 +0900

    feat: JSON 데이터를 자바 객체로 역직렬화 하기 위해 @JsonValue, @JsonCreator 추가

--- module-core/src/main/java/com/zunza/yeogida/constant/MemberType.java
+++ module-core/src/main/java/com/zunza/yeogida/constant/MemberType.java
@@ -1,9 +1,30 @@
 package com.zunza.yeogida.constant;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum MemberType {
 	GUEST("게스트"),
 	HOST("호스트");
 
-	MemberType(String value){
+	private String value;
+
+	MemberType(String value) {
+		this.value = value;
+	}
+
+	@JsonValue
+	public String getValue() {
+		return value;
+	}
+
+	@JsonCreator
+	public MemberType form(String memberType) {
+		for (MemberType type : MemberType.values()) {
+			if (type.getValue().equals(memberType)) {
+				return type;
+			}
+		}
+		return null;
 	}
 }

6f0eb481cad42f1379e86684a8bcf636fdcaa887
zunza <fksfks015@naver.com>
2024-10-19 00:49:09 +0900

    feat: 멤버 객체 생성을 위한 정적 팩토리 메서드 추가

--- module-core/src/main/java/com/zunza/yeogida/domain/Member.java
+++ module-core/src/main/java/com/zunza/yeogida/domain/Member.java
@@ -1,12 +1,14 @@
 package com.zunza.yeogida.domain;
 
 import com.zunza.yeogida.constant.MemberType;
+import com.zunza.yeogida.dto.SignupMemberRequestDto;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -31,4 +33,23 @@ public class Member extends BaseEntity {
 	@Column(nullable = false)
 	@Enumerated(EnumType.STRING)
 	private MemberType memberType;
+
+	@Builder
+	private Member(String email, String password, String name, String phone, MemberType memberType) {
+		this.email = email;
+		this.password = password;
+		this.name = name;
+		this.phone = phone;
+		this.memberType = memberType;
+	}
+
+	public static Member from(SignupMemberRequestDto signupMemberRequestDto) {
+		return Member.builder()
+			.email(signupMemberRequestDto.getEmail())
+			.password(signupMemberRequestDto.getPassword())
+			.name(signupMemberRequestDto.getName())
+			.phone(signupMemberRequestDto.getPhone())
+			.memberType(signupMemberRequestDto.getMemberType())
+			.build();
+	}
 }
